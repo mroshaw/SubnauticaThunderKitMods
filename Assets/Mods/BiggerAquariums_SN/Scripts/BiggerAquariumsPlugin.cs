379db8c27fc7014fe4cf7ebc6a0ef06c4b67b452
mroshaw <oli@mroshaw.co.uk>
2025-10-17 13:43:24 +0100

    Renamed BetterAquariums to BiggerAquariums

---
+++ Assets/Mods/BiggerAquariums_SN/Scripts/BiggerAquariumsPlugin.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using BepInEx;
+using DaftAppleGames.ModUtils;
+using HarmonyLib;
+using Nautilus.Handlers;
+
+namespace DaftAppleGames.BiggerAquariums_SN
+{
+    [BepInPlugin(MyGuid, PluginName, VersionString)]
+    public class BiggerAquariumsPlugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "com.mroshaw.BiggerAquariumssn";
+        private const string PluginName = "Better Aquariums SN";
+        private const string VersionString = "1.0.0";
+
+        private const string AssetBundleName = "BiggerAquariumsassetbundle";
+        internal static ModAssetBundleUtils ModAssetUtils;
+        
+        // Config file / UI initialisation
+        private static readonly Harmony Harmony = new Harmony(MyGuid);
+        private static readonly ModConfigFile ConfigFile = OptionsPanelHandler.RegisterModOptions<ModConfigFile>();
+        internal static ModLog ModDebugLog;
+
+        private void Awake()
+        {
+            // Set up logging and asset bundle
+            ModDebugLog =  new ModLog(Logger, ConfigFile.DetailedLogging);
+            ModAssetUtils = new ModAssetBundleUtils(AssetBundleName, Assembly.GetExecutingAssembly(),true, ModDebugLog);
+
+            // Register our prefabs
+            DoubleAquarium.Register();
+            CornerAquarium.Register();
+            // CurvedAquarium.Register();
+            // LShapedAquarium.Register();
+            
+            // Patch in our MOD
+            Harmony.PatchAll();
+            ModDebugLog.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loaded.");
+        }
+    }
+}
---
+++ Assets/Mods/BiggerAquariums_SN/Scripts/ModConfigFile.cs
@@ -0,0 +1,24 @@
+﻿using Nautilus.Json;
+using Nautilus.Options;
+using Nautilus.Options.Attributes;
+
+namespace DaftAppleGames.BiggerAquariums_SN
+{
+    /// <summary>
+    /// Nautilus mod config class
+    /// </summary>
+    [Menu("Bigger Aquariums")]
+    internal class ModConfigFile : ConfigFile
+    {
+        [Toggle("Detailed Logging", Tooltip="Only check this if you have a problem and need to see the debug output of the mod in the Player.log file"), OnChange(nameof(DetailedLoggingChangedHandler))]
+        public bool DetailedLogging = false;
+
+        /// <summary>
+        /// Set the Detailed Logging on the Mod Logger
+        /// </summary>
+        private void DetailedLoggingChangedHandler(ToggleChangedEventArgs newArgs)
+        {
+            BiggerAquariumsPlugin.ModDebugLog.SetDetailedLoggingState(newArgs.Value);
+        }
+    }
+}
